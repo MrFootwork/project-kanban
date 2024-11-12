12358914031d6b5a7a6135b238f31ec5813aac71
Mr Footwork <pandau.ting@gmail.com>
2024-11-12 17:10:24 +0100

    🛠 add Footer

--- project-kanban/src/components/Footer.jsx
+++ project-kanban/src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import '../assets/css/Footer.css'
+import '../assets/css/Footer.css';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<div id='footer'>
+			<p>
+				Made with <span className='fa fa-heart' /> by Malek and Pandau.
+			</p>
+			<p>
+				To visit our repo click{' '}
+				<a href='https://github.com/MrFootwork/project-kanban'>here</a>.
+			</p>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
