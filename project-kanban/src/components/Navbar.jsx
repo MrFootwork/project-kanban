bbab5ff06b8f1e0a7d3642c6c4617e0034c846ff
Mr Footwork <pandau.ting@gmail.com>
2024-11-12 18:02:54 +0100

    🧹 cleanup

--- project-kanban/src/components/Navbar.jsx
+++ project-kanban/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import '../assets/css/Navbar.css' ;
-import LogoImage from '../assets/css/logo.png';
+import logoImage from '../assets/images/logo.png';
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img id="logo" src={LogoImage} alt='Logo Image' />
-      <h1 id='app-name'>kannab</h1>
+      <img id="logo" src={logoImage} alt='Logo Image' />
+      <h1 id='app-title'>kannab</h1>
     </div>
   )
 }
