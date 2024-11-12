6317752a0494e00127a945741f618f77d43939a8
Malek T <malek_tatin@yahoo.com>
2024-11-12 17:18:42 +0100

    Navbar

--- project-kanban/src/components/Navbar.jsx
+++ project-kanban/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import '../assets/css/Navbar.css'
+import '../assets/css/Navbar.css' ;
+import LogoImage from '../assets/css/logo.png';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img id="logo" src={LogoImage} alt='Logo Image' />
+      <h2 id='app-name'>kannab</h2>
+    </div>
   )
 }
 
