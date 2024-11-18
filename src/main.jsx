18c6c94269f3a1c7f57f4689a034256d44f58bb5
Mr Footwork <pandau.ting@gmail.com>
2024-11-18 23:23:53 +0100

    always use base url for routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename='/project-kanban'>
 			<App />
 		</BrowserRouter>
 	</StrictMode>
